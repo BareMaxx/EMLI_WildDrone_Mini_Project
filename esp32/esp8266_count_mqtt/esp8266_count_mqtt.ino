b5dfd5f05c24206aff2bfc0bc15d4798e8cc2e67
Mikkel-Github <mikkelpriisholm@gmail.com>
2024-05-13 14:19:25 +0200

    connected mqtt and button posts to mqtt

---
+++ esp32/esp8266_count_mqtt/esp8266_count_mqtt.ino
@@ -0,0 +1,167 @@
+
+
+// Embedded Linux (EMLI)
+// University of Southern Denmark
+
+// 2022-03-24, Kjeld Jensen, First version
+
+// Configuration
+#define WIFI_SSID       "EMLI_TEAM_11" // 
+#define WIFI_PASSWORD    "SecureTheStash"
+
+#define MQTT_SERVER      "io.adafruit.com"
+#define MQTT_SERVERPORT  1883 
+#define MQTT_USERNAME    "priisholm"
+#define MQTT_KEY         "aio_jEcP16br6erABdP8IWdIk59eBpY4"
+#define MQTT_TOPIC       "/feeds/external-trigger"
+
+// wifi
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+ESP8266WiFiMulti WiFiMulti;
+const uint32_t conn_tout_ms = 5000;
+
+// counter
+#define GPIO_INTERRUPT_PIN 4
+#define DEBOUNCE_TIME 100 
+volatile unsigned long count_prev_time;
+volatile unsigned long count;
+
+// mqtt
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+WiFiClient wifi_client;
+Adafruit_MQTT_Client mqtt(&wifi_client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_KEY);
+Adafruit_MQTT_Publish count_mqtt_publish = Adafruit_MQTT_Publish(&mqtt, MQTT_USERNAME MQTT_TOPIC);
+
+// publish
+#define PUBLISH_INTERVAL 30000
+unsigned long prev_post_time;
+
+// debug
+#define DEBUG_INTERVAL 2000
+unsigned long prev_debug_time;
+
+void debug(const char *s)
+{
+  Serial.print (millis());
+  Serial.print (" ");
+  Serial.println(s);
+}
+
+void mqtt_connect()
+{
+  int8_t ret;
+
+  // Stop if already connected.
+  if (! mqtt.connected())
+  {
+    debug("Connecting to MQTT... ");
+    while ((ret = mqtt.connect()) != 0)
+    { // connect will return 0 for connected
+         Serial.println(mqtt.connectErrorString(ret));
+         debug("Retrying MQTT connection in 5 seconds...");
+         mqtt.disconnect();
+         delay(5000);  // wait 5 seconds
+    }
+    debug("MQTT Connected");
+  }
+}
+
+void print_wifi_status()
+{
+  Serial.print (millis());
+  Serial.print(" WiFi connected: ");
+  Serial.print(WiFi.SSID());
+  Serial.print(" ");
+  Serial.print(WiFi.localIP());
+  Serial.print(" RSSI: ");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
+}
+
+void setup()
+{
+  // count
+  count_prev_time = millis();
+  count = 0;
+  pinMode(GPIO_INTERRUPT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(GPIO_INTERRUPT_PIN), count_isr, RISING);
+
+  // serial
+  Serial.begin(115200);
+  delay(10);
+  debug("Boot");
+
+  // wifi
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  if(WiFiMulti.run(conn_tout_ms) == WL_CONNECTED)
+  {
+    print_wifi_status();
+
+    mqtt_connect();
+  }
+  else
+  {
+    debug("Unable to connect");
+  }
+}
+
+void publish_data()
+{
+  Serial.println("publish_data");
+  // char payload[10];
+  char eventChar[6] = "event";
+  //sprintf (payload, "%s", *eventChar);
+  Serial.print(millis());
+  Serial.print(" Publishing: ");
+  Serial.println(eventChar);
+
+  Serial.print(millis());
+  Serial.println(" Connecting...");
+  if((WiFiMulti.run(conn_tout_ms) == WL_CONNECTED))
+  {
+    print_wifi_status();
+  
+    mqtt_connect();
+    if (! count_mqtt_publish.publish(eventChar))
+    {
+      debug("MQTT failed");
+    }
+    else
+    {
+      debug("MQTT ok");
+    }
+  }
+}
+
+ICACHE_RAM_ATTR void count_isr()
+{
+  if (count_prev_time + DEBOUNCE_TIME < millis() || count_prev_time > millis())
+  {
+    count_prev_time = millis(); 
+    count++;
+
+    publish_data();
+  }
+}
+
+void loop()
+{
+    // if (millis() - prev_post_time >= PUBLISH_INTERVAL)
+    // {
+    //   prev_post_time = millis();
+    //   publish_data();
+    // }
+   
+    // if (millis() - prev_debug_time >= DEBUG_INTERVAL)
+    // {
+    //   prev_debug_time = millis();
+    //   Serial.print(millis());
+    //   Serial.print(" ");
+    //   Serial.println(count);
+    // }
+}
+
