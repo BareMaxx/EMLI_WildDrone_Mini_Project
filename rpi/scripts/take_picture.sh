68c588c177a7c095ca195e5a7ed48d3342b54803
alex123a <alex.valentiner@hotmail.com>
2024-05-15 10:50:32 +0200

    Saving picture in motion_pictures directory

--- rpi/scripts/take_picture.sh
+++ rpi/scripts/take_picture.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+cd pictures/
+
 take_picture() {
-	cd pictures/
 	a=$(ls -t "./" | tail -n 1)
 	echo "File to be overwritten: $a"
 	sudo rpicam-still -t 0.01 -o "$a"
@@ -9,12 +10,20 @@ take_picture() {
 
 motion_detection() {
 	cd ..
-	python motion_detect.py ./pictures/picture1.jpg ./pictures/picture2.jpg
+	motion=$(python motion_detect.py ./pictures/picture1.jpg ./pictures/picture2.jpg)
+	echo "$motion"
+	motion_detected="Motion detected"
+	if [ "$motion" = "$motion_detected" ]; then
+		echo "test of if statement"
+		cp ./pictures/"$a" ./motion_pictures/"$(date '+%Y-%m-%d_%H:%M:%S')_$a"
+	else
+		echo "No picture saved"
+	fi
 }
 
 
 while true; do
 	take_picture
-	motion_detection &
+	motion_detection "$a" &
 	sleep 3
 done
