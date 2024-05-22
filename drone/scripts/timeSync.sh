fb8677d62067d50aa1fdcacc570f117f1c04787e
mikkel-github <mikkelpriisholm@gmail.com>
2024-05-22 14:28:50 +0200

    drone master script and time sync

--- drone/scripts/timeSync.sh
+++ drone/scripts/timeSync.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 server_ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+echo "Server's IP: $server_ip"
 
 client_ip="10.0.0.10" #localhost
 client_user="jeinere"
