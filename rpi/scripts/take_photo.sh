6f391f08b2509a4f42480602398d9ecc04511e8c
Max Hansen <maxx0758@gmail.com>
2024-05-22 13:44:33 +0200

    Add logging to motion_detection and take_photo

--- rpi/scripts/take_photo.sh
+++ rpi/scripts/take_photo.sh
@@ -1,5 +1,5 @@
 trigger="$1"
-
+sh /logger.sh DEBUG "TAKE_PHOTO" "Triggered by $trigger"
 milli=$(date '+%3N')
 date_now=$(date '+%Y-%m-%d')
 name="$(date '+%H%M%S')_$milli"
@@ -7,9 +7,14 @@ create_date="$(date '+%Y-%m-%d %H:%M:%S').$milli$(date '+%:z')"
 date_time=$(date '+%Y-%m-%d %H:%M:%S')
 
 sudo mkdir -p ../../data/pictures/"$date_now"
+sh /logger.sh DEBUG "TAKE_PHOTO" "Created folder: $date_now"
+
 sudo chmod -R 755 ../../data/pictures/"$date_now"
 picturePath=../../data/pictures/"$date_now"/"$name.jpg"
+sh /logger.sh DEBUG "TAKE_PHOTO" "Taking photo"
 sudo rpicam-still -t 0.01 -o "$picturePath"
+
+sh /logger.sh DEBUG "TAKE_PHOTO" "Photo saved in $date_now/$name.jpg"
 
 subject_distance=$(exiftool -s -SubjectDistance "$picturePath")
 exposure_time=$(exiftool -s -ExposureTime "$picturePath")
@@ -25,5 +30,9 @@ json_object='{
 "ISO": "'"$(echo "$iso" | awk '{print $NF}')"'"
 }'
 
+
+
 echo "$json_object" > ../../data/pictures/"$date_now"/"$name.json"
+
+sh /logger.sh DEBUG "TAKE_PHOTO" "JSON saved in: $date_now/$name.json"
 
