7c27618c3d569de3d9dfdd5792642a7308821920
Max Hansen <maxx0758@gmail.com>
2024-05-23 10:27:51 +0200

    Made the shellscripts on the RPI know where it's own location is. This is important to fix the relative paths used in the script, when the script is ran in crontab. Also added username and password for the MQTT in the rain_handler as those were missing.

--- rpi/scripts/rain_handling/activate_rain_publisher.sh
+++ rpi/scripts/rain_handling/activate_rain_publisher.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-sh ../logger.sh DEBUG "RAIN_DETERMINER" "Initializing"
-mosquitto_sub -t rain_sensed |
+script_dir=$(dirname "$(readlink -f "$0")")
+sh $script_dir/../logger.sh DEBUG "RAIN_DETERMINER" "Initializing"
+mosquitto_sub -u "my_user" -P "SecureTheStash" -t rain_sensed |
    while read payload ; do
       
       if echo "$payload" | grep -q '"rain_detect": 1'; then
-         sh ../logger.sh DEBUG "RAIN_DETERMINER" "Rain detected"
-         sh ../logger.sh DEBUG "RAIN_DETERMINER" "Publishing to wiper"
-         mosquitto_pub -h localhost -t activate_wiper -m "activate_wiper"
+         sh $script_dir/../logger.sh DEBUG "RAIN_DETERMINER" "Rain detected"
+         sh $script_dir/../logger.sh DEBUG "RAIN_DETERMINER" "Publishing to wiper"
+         mosquitto_pub -u "my_user" -P "SecureTheStash" -h localhost -t activate_wiper -m "activate_wiper"
       fi
 
    done
