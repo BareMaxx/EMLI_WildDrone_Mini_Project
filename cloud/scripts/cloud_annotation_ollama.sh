89a79f7bbbcef2f3ea30eaf33566436fd50bbc90
Alex Valentiner <alex.valentiner@hotmail.com>
2024-05-23 15:01:53 +0200

    Done with cloud

--- cloud/scripts/cloud_annotation_ollama.sh
+++ cloud/scripts/cloud_annotation_ollama.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
 directory_path="$1"
-
 script_dir=$(dirname "$(readlink -f "$0")")
 
+echo "Test1: $directory_path"
 for file_path in "$directory_path"/*; do
-	if [[ "$file_path" == *"json"* ]]; then
-		picture_path="${file_path%.*}.jpg"
- 		json_file="$file_path"
+	echo "Test2: $file_path"
+        if [[ "$file_path" == *"json"* ]]; then
+                picture_path="${file_path%.*}.jpg"
+                json_file="$file_path"
 
-		response=$(sudo ollama run llava "Describe $picture_path")
-		echo "Test of response: $response"
-		annotation_text="$response"
+                if jq -e '.["Annotation"] | has("Source")' "$json_file" >/dev/null; then
+                        echo "Test3"
+			continue
+                else
+                        response=$(sudo ollama run llava:7b "Describe $picture_path")
+                        echo "Test of response: $response"
+                        annotation_text="$response"
 
-		new_data=$(jq -n --arg source "Llama3" --arg text "$annotation_text" \
-        	    '{"Annotation": {"Source": $source, "Text": $text}}')
+                        new_data=$(jq -n --arg source "llava:7b" --arg text "$annotation_text" \
+                        '{"Annotation": {"Source": $source, "Text": $text}}')
 
-		jq --argjson new_data "$new_data" '. + $new_data' "$json_file" > tmp.$$>
-	fi
+			jq --argjson new_data "$new_data" '. + $new_data' "$json_file" > "tmp.$$.json" && mv "tmp.$$.json" "$json_file"
+                fi
+        fi
 done
 
-sudo mkdir -P $script_dir/../../data/cloud_json/
+
+sudo mkdir -p $script_dir/../../data/cloud_json/
 sudo cp "$directory_path"/*.json $script_dir/../../data/cloud_json/
